b9a7d30b714c9f41a907743c439886eb0b9373a4
apm29 <yjw999wow@163.com>
2019-05-30 18:05:14 +0800

    wip:provider research

--- lib/ui/test_page.dart
+++ lib/ui/test_page.dart
@@ -10,11 +10,132 @@ import '../index.dart';
 import '../main.dart';
 import '../utils.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: TestPage(),
+    home: ProviderTestPage(),
   ));
+}
+
+class ProviderTestPage extends StatefulWidget {
+  @override
+  _ProviderTestPageState createState() => _ProviderTestPageState();
+}
+
+class CartModel extends ChangeNotifier {
+  double price = 0.0;
+
+  void add(double newValue) {
+    price += newValue;
+    notifyListeners();
+  }
+
+  List<Item> items = [];
+
+  void addItem(Item item) {
+    var contain = items.firstWhere((v) => item.id == v.id, orElse: () => null);
+    if (contain == null) {
+      items.add(item);
+    } else {
+      contain.count += item.count;
+    }
+  }
+}
+
+class Item {
+  double price;
+  int id;
+  double count;
+
+  Item(this.price, this.id, this.count);
+}
+
+class _ProviderTestPageState extends State<ProviderTestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<CartModel>(
+      builder: (context) {
+        return CartModel();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Consumer<CartModel>(
+            builder: (context, value, widget) {
+              return Text(value.price.toStringAsFixed(2));
+            },
+          ),
+          actions: <Widget>[
+            Consumer<CartModel>(
+              builder: (context, value, widget) {
+                return FlatButton.icon(
+                    onPressed: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return CartPage(value.items);
+                      }));
+                    },
+                    icon: Icon(
+                      Icons.attach_money,
+                      color: Colors.white,
+                    ),
+                    label: Text(
+                      "Buy",
+                      style: TextStyle(color: Colors.white),
+                    ));
+              },
+            )
+          ],
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            Item item = Item(Random(12).nextDouble() * 100, index, 1.0);
+            return FlatButton(
+              onPressed: () {
+                Provider.of<CartModel>(context).addItem(item);
+                Provider.of<CartModel>(context).add(item.price * item.count);
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                    "${index.toString()} ---- \$${item.price.toStringAsFixed(2)}"),
+              ),
+            );
+          },
+          itemCount: 21,
+        ),
+      ),
+    );
+  }
+}
+
+class CartPage extends StatefulWidget {
+  final List<Item> lists;
+
+  CartPage(this.lists);
+
+  @override
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          var item = widget.lists[index];
+          return ListTile(
+            leading: Text(item.id.toString()),
+            title: Text(item.price.toStringAsFixed(2)),
+            subtitle: Text("x ${item.count.toStringAsFixed(2)}"),
+          );
+        },
+        itemCount: widget.lists.length,
+      ),
+    );
+  }
 }
 
 class TestPage extends StatefulWidget {
@@ -37,21 +158,22 @@ class _TestPageState extends State<TestPage> {
             child: FutureBuilder<String>(
               builder: (context, snapshot) {
                 return InkWell(
-                  onLongPress: (){
-                    Clipboard.setData(ClipboardData(text:snapshot.data));
-                    getExternalStorageDirectory().then((d){
+                  onLongPress: () {
+                    Clipboard.setData(ClipboardData(text: snapshot.data));
+                    getExternalStorageDirectory().then((d) {
                       File f = File("${d.absolute.path}/base64.txt");
                       print('${f.path}');
                       f.createSync();
-                      f.writeAsString(snapshot.data,flush: true);
+                      f.writeAsString(snapshot.data, flush: true);
                     });
                   },
                   child: Text(
-                     snapshot.data,
+                    snapshot.data,
                   ),
                 );
               },
-              future: ImagePicker.pickImage(source: ImageSource.gallery).then((f){
+              future:
+                  ImagePicker.pickImage(source: ImageSource.gallery).then((f) {
                 return getImageBase64(f);
               }),
             ),
@@ -61,7 +183,7 @@ class _TestPageState extends State<TestPage> {
     );
   }
 
-  Future<int> someFuture()async{
+  Future<int> someFuture() async {
     await Future.delayed(Duration(seconds: 2));
     return 1;
   }
